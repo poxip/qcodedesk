5311ade6fc43cfc4e33fe17cf33d37ea4ca381d8
MrPoxipol <michal.proszek@gmail.com>
2014-06-28 10:32:15 +0200

    News class

--- include/News.h
+++ include/News.h
@@ -1,10 +1,27 @@
+/**
+  * @author MrPoxipol <michal.proszek@gmail.com>
+  * @date   Jun 26, 2014
+  * @brief  Polls cpp0x.pl for rss changes and produces news info.
+  */
+
 #ifndef NEWS_H
 #define NEWS_H
+
+#include <QString>
+#include <vector>
 
 class News
 {
 public:
     News();
+
+    /**
+     * @brief fetches cpp0x.pl/xml and updates news data
+     * @return \btrue if update was complete successfull, otherwise \bfalse
+     */
+    bool update();
+
+    std::vector <Topic> topics;
 };
 
 #endif // NEWS_H
--- src/News.cpp
+++ src/News.cpp
@@ -1,5 +1,17 @@
+/**
+  * @author MrPoxipol <michal.proszek@gmail.com>
+  * @date   Jun 26, 2014
+  * @brief  MainWindow implementation.
+  */
+
 #include "News.h"
 
 News::News()
 {
+}
+
+/** \copydoc update */
+bool News::update()
+{
+
 }
